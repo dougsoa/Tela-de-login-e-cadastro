f9667508d5b620df0fa945aa782bd360d828aa85
Douglas Oliveira <83471539+dougsoa@users.noreply.github.com>
2021-09-13 16:17:22 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var btnSignin = document.querySelector('#signin')
+var btnSignup = document.querySelector('#signup')
+
+var body = document.querySelector('body')
+
+btnSignin.addEventListener('click', function () {
+  body.className = 'sig-in-js'
+})
+
+btnSignup.addEventListener('click', function () {
+  body.className = 'sig-up-js'
+})
